1f8117a82c377c826f08eb8588917a2bd4e2b468
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2024-08-21 16:21:09 +0600

    examples upto 26 have been completed

--- js/script.js
+++ js/script.js
@@ -67,4 +67,78 @@ $(document).ready(function(){
         $('#toggle_text_mouseover').toggle();
     })
 
+    // example-12
+    $("#fadeIn_btn").on("click", function(){
+    $('#fadeIn').fadeIn();
+    })
+
+    // example-13
+    $("#fadeInFast_btn").on("click", function(){
+        $("#fadeInFast").fadeIn('fast');
+    })
+
+    // example-14
+    $("#fadeInSlow_btn").on("click", function(){
+        $("#fadeInSlow").fadeIn("slow");
+    })
+
+    // example-15
+    $("#fadeInMilliSeconds_btn").on("click", function(){
+        $("#fadeInMilliSeconds").fadeIn(5000);
+    })
+
+    // example-16
+    $("#fadeOut_btn").on("click", function(){
+    $('#fadeOut').fadeOut();
+    })
+
+    // example-17
+    $("#fadeOutFast_btn").on("click", function(){
+        $("#fadeOutFast").fadeOut('fast');
+    })
+
+    // example-18
+    $("#fadeOutSlow_btn").on("click", function(){
+        $("#fadeOutSlow").fadeOut("slow");
+    })
+
+    // example-19
+    $("#fadeOutMilliSeconds_btn").on("click", function(){
+        $("#fadeOutMilliSeconds").fadeOut(5000);
+    })
+    // example-20
+    $("#fadeToggle_btn").on("click", function(){
+    $('#fadeToggle').fadeToggle();
+    })
+
+    // example-21
+    $("#fadeToggleFast_btn").on("click", function(){
+        $("#fadeToggleFast").fadeToggle('fast');
+    })
+
+    // example-22
+    $("#fadeToggleSlow_btn").on("click", function(){
+        $("#fadeToggleSlow").fadeToggle("slow");
+    })
+
+    // example-23
+    $("#fadeToggleMilliSeconds_btn").on("click", function(){
+        $("#fadeToggleMilliSeconds").fadeToggle(5000);
+    })
+
+    // example-24
+    $("#fadeTo_btn1").on('click', function(){
+        $('#fadeTo1').fadeTo('slow', 0.25)
+    })
+
+    // example-25
+    $("#fadeTo_btn2").on('click', function(){
+        $('#fadeTo2').fadeTo('slow', 0.50)
+    })
+
+    // example-26
+    $("#fadeTo_btn3").on('click', function(){
+        $('#fadeTo3').fadeTo('slow', 0.75)
+    })
+
 })
